bc292b2859cbebb3f962db218d118072b69bcc37
kaine0x00 <kaine0x00@gmail.com>
2023-12-03 16:47:18 +0100

    final ver

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll('a[href^="#"]');
+
+  links.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop - 170, // Adjust this value to match the height of your fixed menu
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+});
